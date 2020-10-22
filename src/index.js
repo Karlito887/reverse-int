ca34b51e13367104fb473afed705d4ff204d8b5c
Nazarii Karlyk <karliknazar@gmail.com>
2020-10-22 21:28:55 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reverseString = n.toString()
+        .split('-')
+        .join('')
+        .split('')
+        .reverse()
+        .join('')
+    return + reverseString
 }
+
+
+// -192 ------------- 291    
